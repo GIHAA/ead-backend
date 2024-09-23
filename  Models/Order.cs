b6762616a0aeb341ba0e9a3070a64a34bbff49c6
Sachini Tharuka <sachinitkandambige29@gmail.com>
2024-09-23 11:25:06 +0530

    wip

---  Models/Order.cs
+++  Models/Order.cs
@@ -11,27 +11,19 @@ namespace TechFixBackend._Models
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
 
-        
         [BsonRepresentation(BsonType.ObjectId)]
         public string CustomerId { get; set; }
 
-        
         public List<OrderItem> Items { get; set; }
 
-       
         public DateTime OrderDate { get; set; } = DateTime.UtcNow;
 
         public string Status { get; set; } = "Processing";
 
-       
         public float TotalAmount { get; set; }
 
         public string DeliveryAddress { get; set; }
 
-        
-        public string DeliveryStatus { get; set; }
-
-        
         public DateTime? DispatchedDate { get; set; }
     }
 
@@ -41,16 +33,12 @@ namespace TechFixBackend._Models
         [BsonRepresentation(BsonType.ObjectId)]
         public string ProductId { get; set; }
 
-      
         public int Quantity { get; set; }
 
-       
         public float Price { get; set; }
 
-     
         public string Status { get; set; } = "Processing";
 
-        
         public float TotalPrice => Quantity * Price;
     }
 }
