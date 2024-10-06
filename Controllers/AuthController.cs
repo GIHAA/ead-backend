96501034d4de95f1622cf5702f97549d8d86c3c1
CoorayNTL <tl685998@gmail.com>
2024-10-07 03:36:20 +0530

    Dependenci changes

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -302,19 +302,19 @@ namespace TechFixBackend.Controllers
         }
 
 
-        //get user by id
-        [HttpGet("user/{id}")]
-        public async Task<IActionResult> GetUser(string id)
-        {
-            try
-            {
-                var user = await _authService.GetUserByIdAsync(id);
-                return Ok(user);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { Message = ex.Message });
-            }
-        }
+        ////get user by id
+        //[HttpGet("user/{id}")]
+        //public async Task<IActionResult> GetUser(string id)
+        //{
+        //    try
+        //    {
+        //        var user = await _authService.GetUserByIdAsync(id);
+        //        return Ok(user);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(new { Message = ex.Message });
+        //    }
+        //}
     }
 }
