a89b407338864c98e1ffb131da4c912f90fc98fa
th-perera <it21165498@my.sliit.lk>
2024-10-08 00:19:38 +0530

    Feat: feedback get and update

--- Repository/IFeedbackRepository .cs
+++ Repository/IFeedbackRepository .cs
@@ -9,6 +9,7 @@ namespace TechFixBackend.Repository
         Task<List<Feedback>> GetFeedbackByVendorIdAsync(string vendorId);
         Task<List<Feedback>> GetFeedbackByProductIdAsync(string productId);
         Task<List<Feedback>> GetFeedbacksAsync(int pageNumber, int pageSize, string search = "");
+        Task<Feedback> GetFeedbackForCustomerProductVendorAsync(string vendorId, string productId, string customerId);
         Task<long> GetTotalFeedbacksAsync(string search = "");
         Task<Feedback> GetFeedbackByIdAsync(string feedbackId);
         Task UpdateFeedbackAsync(string feedbackId, string customerId, string newComment, float newRating);
